f41d65b031a9b7c221cc87321d1425bb80d9d4a0
kj <79355765+kj27july@users.noreply.github.com>
2021-06-12 03:37:28 +0530

    Again written code as last one got deleted

--- app/src/main/java/com/example/countonoffbluetooth/MainActivity.kt
+++ app/src/main/java/com/example/countonoffbluetooth/MainActivity.kt
@@ -1,22 +1,19 @@
 package com.example.countonoffbluetooth
 
-
 import android.bluetooth.BluetoothAdapter
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
+import android.content.*
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    var  onCount=0
-    var  offCount=0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        displayCount()
     }
 
     override fun onResume() {
@@ -24,29 +21,60 @@ class MainActivity : AppCompatActivity() {
         registerReceiver(bluetoothReceiver,  IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED))
     }
 
+    fun updateCountTextView(t1: String?,t2: String?) {
+        runOnUiThread {
+            val textV1 : TextView  = findViewById<View>(R.id.onC) as TextView
+            textV1.text = t1
+
+            val textV2 : TextView  = findViewById<View>(R.id.offC) as TextView
+            textV2.text = t2
+        }
+    }
+
+    fun displayCount()
+    {
+        var helper=CountDBHelper2(applicationContext)
+        var db=helper.readableDatabase
+        var rs=db.rawQuery("select * from COUNTONOFF",null)
+        if(rs.moveToNext())
+        {
+            updateCountTextView(rs.getString(0),rs.getString(1))
+            Toast.makeText(applicationContext,"On Count:"+rs.getString(0)+" Off Count:"+rs.getString(1),Toast.LENGTH_LONG).show()
+        }
+    }
+
+    fun updateOnCount()
+    {
+        var helper=CountDBHelper2(applicationContext)
+        var db=helper.readableDatabase
+        var rs=db.rawQuery("update COUNTONOFF set OnCount=1+(select OnCount from COUNTONOFF)",null)
+        rs.moveToNext()
+        displayCount()
+    }
+
+    fun updateOffCount()
+    {
+        var helper=CountDBHelper2(applicationContext)
+        var db=helper.readableDatabase
+        var rs=db.rawQuery("update COUNTONOFF set OffCount=1+(select OffCount from COUNTONOFF)",null)
+        rs.moveToNext()
+        displayCount()
+    }
+
     private val bluetoothReceiver: BroadcastReceiver = object : BroadcastReceiver() {
-        lateinit var onCC:TextView
-        lateinit var offCC:TextView
         override fun onReceive(context: Context, intent: Intent) {
             setContentView(R.layout.activity_main)
             val action = intent.action
             if (BluetoothAdapter.ACTION_STATE_CHANGED == action) {
                 when (intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, -1)) {
-                    BluetoothAdapter.STATE_OFF -> {
-                        offCount+=1
-                        offCC =findViewById <TextView>(R.id.offC)
-                        offCC.text= offCount.toString()
-                        Toast.makeText(this@MainActivity, "Bluetooth is off,Off Count:" + offCount, Toast.LENGTH_SHORT).show()
-                    }
                     BluetoothAdapter.STATE_ON -> {
-                        onCount+=1
-                          onCC =findViewById <TextView>(R.id.onC)
-                        onCC.text= onCount.toString()
-                        Toast.makeText(this@MainActivity, "Bluetooth is on, On Count:" + onCount, Toast.LENGTH_SHORT).show()
+                        updateOnCount()
+                                                 }
+                    BluetoothAdapter.STATE_OFF -> {
+                        updateOffCount()
                     }
                 }
             }
         }
     }
-
 }
